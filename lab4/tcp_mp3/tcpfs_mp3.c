0f9e4354f095928f7ed3e7d6d7a3eb584576595f
coe18b066 <coe18b066@iiitd.ac.in>
2020-09-14 11:22:04 +0530

    initial commit

---
+++ lab4/tcp_mp3/tcpfs_mp3.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<unistd.h>
+
+int main()
+{
+	int s_sock,c_client;
+	char buf[100];
+	char buf2[100];
+	s_sock=socket(AF_INET,SOCK_STREAM,0);
+	struct sockaddr_in server,other;
+	memset(&server,0,sizeof(server));
+	memset(&other,0,sizeof(other));
+	server.sin_family=AF_INET;
+	server.sin_port=htons(9009);
+	server.sin_addr.s_addr=INADDR_ANY;
+	socklen_t addr_size;
+	bind(s_sock,(struct sockaddr*)&server,sizeof(server));
+	listen(s_sock,5);
+	addr_size=sizeof(other);
+	c_client=accept(s_sock,(struct sockaddr*)&other,&addr_size);
+
+	
+	FILE *fp;
+	fp=fopen("sample1.mp3","wb");
+	int ch[1];
+	
+	read(c_client,ch,sizeof(ch));
+	while(ch[0]!=EOF)
+	{
+		fputc(ch[0],fp);
+		read(c_client,ch,sizeof(ch));
+	}
+	printf("\nfile received successfully\n");
+	printf("data copied into file sample1.mp3\n");
+	
+	fclose(fp);
+	close(s_sock);
+	return 0;
+}
