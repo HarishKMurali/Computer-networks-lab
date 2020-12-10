183e5e2dc81b71b0281de899dcb5ec80c330f3b9
coe18b066 <coe18b066@iiitd.ac.in>
2020-12-10 11:01:50 +0530

    bmp file transfer added

---
+++ lab6/udp_bmp/udpf.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<unistd.h>
+#include<time.h>
+int main()
+{
+	int c_sock;
+	char buf[50];
+	char bufr[20];
+	c_sock=socket(AF_INET,SOCK_DGRAM,0);
+	struct sockaddr_in client;
+	client.sin_family=AF_INET;
+	client.sin_port=htons(9009);
+	client.sin_addr.s_addr=INADDR_ANY;
+
+	FILE *fp;
+	fp=fopen("bitmap_img.bmp","rb");;
+	if(fp==NULL)
+	{
+		printf("\nfile doesnot exist!\n");
+		exit(1);
+	}
+	strcpy(buf,"connected\nwait;sending in process\n");
+	printf("connected\n");
+	sendto(c_sock,buf,sizeof(buf),0,(struct sockaddr*)&client,sizeof(client));
+
+	int ch[1];
+	clock_t start,end;
+	double cpu_time_used;
+	start= clock();
+	
+	while((ch[0]=fgetc(fp))!=EOF)
+	{
+		sendto(c_sock,ch,sizeof(ch),0,(struct sockaddr*)&client,sizeof(client));
+		sleep(0.01);
+	}
+	ch[0]=EOF;
+
+	sendto(c_sock,ch,sizeof(ch),0,(struct sockaddr*)&client,sizeof(client));
+	printf("\nbitmap file sent successfully\n");
+	end=clock();
+	cpu_time_used=((double)(end-start))/CLOCKS_PER_SEC;
+	printf("Time taken for file transfer %f\n",cpu_time_used);
+	fclose(fp);
+
+	return 0;
+}
---
+++ lab6/udp_bmp/udpfs.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<string.h>
+
+int main()
+{
+	int s_sock,c_client;
+	char buf[20];
+	char buf2[20];
+	s_sock=socket(AF_INET,SOCK_DGRAM,0);
+	struct sockaddr_in server,other;
+	server.sin_family=AF_INET;
+	server.sin_port=htons(9009);
+	server.sin_addr.s_addr=INADDR_ANY;
+	int k=bind(s_sock,(struct sockaddr*)&server,sizeof(server));
+	if(k==-1)
+	{
+		printf("connection failed!\n");
+		exit(0);
+	}
+	socklen_t add;
+	add=sizeof(other);
+	int n=recvfrom(s_sock,buf,sizeof(buf),0,(struct sockaddr*)&other,&add);
+	printf("msg from client: %s\n",buf);
+
+	FILE *fp;
+	fp=fopen("bitmap_copy.bmp","wb");
+	int ch[1];
+
+	n=recvfrom(s_sock,ch,sizeof(ch),0,(struct sockaddr*)&other,&add);
+	while(ch[0]!=EOF)
+	{
+	fputc(ch[0],fp);
+	n=recvfrom(s_sock,ch,sizeof(ch),0,(struct sockaddr*)&other,&add);
+	}
+	printf("\nfile received successfully\n");
+	printf("data copied into file bitmap_copy.bmp\n");
+
+	fclose(fp);
+
+	return 0;
+}
